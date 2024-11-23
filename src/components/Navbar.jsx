546b2543d6e07a3c959a786e204f8ff853ca6edb
Kamasah Edwin Jnr <kamassahe5@gmail.com>
2024-11-23 14:36:35 +0000

    nav setup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import logo from '../assets/images/logo.png'
+import { Link } from 'react-router-dom'
+import { FaBars } from 'react-icons/fa'
 
 const Navbar = () => {
-  return <nav className='bg-red-500'>navbar</nav>
+  const navElements = [
+    { link: '/', linkName: 'Home' },
+    { link: '/about-us', linkName: 'About Us' },
+    { link: '/services', linkName: 'Services' },
+    { link: '/booking', linkName: 'Booking' },
+    { link: '/contact-us', linkName: 'Contact Us' },
+  ]
+
+  return (
+    <nav className='bg-red-500 flex justify-between'>
+      <div className=''>
+        <img
+          src={logo}
+          alt='next-glo-logo'
+          width={100}
+          height={100}
+          className=''
+        />
+      </div>
+
+      <div className='space-x-4 flex justify-end'>
+        {navElements.map((item) => (
+          <Link key={item.link} to={item.link}>
+            {item.linkName}
+          </Link>
+        ))}
+      </div>
+
+      <FaBars />
+    </nav>
+  )
 }
 
 export default Navbar
