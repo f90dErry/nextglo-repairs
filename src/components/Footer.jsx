d04cfb177910e355c953bd06ca129a53754084e5
Kamasah Edwin Jnr <kamassahe5@gmail.com>
2024-11-24 02:25:12 +0000

    some mint changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -34,27 +34,58 @@ const socialIcons = [
 const Footer = () => {
   const footerYear = new Date().getFullYear()
   return (
-    <footer className='bg-blue-950 text-white bottom-0 fixed w-full py-4 font-sans shadow-[0_-4px_6px_-1px_rgba(0,0,0,0.1)] '>
-      <div className=''>
-        <p>Follow Us</p>
-        {socialIcons.map((item) => (
-          <Link
-            to={item.path}
-            className='flex items-center gap-2 text-gray-400'
-          >
-            {item.icon} {item.name}
-          </Link>
-        ))}
-      </div>
+    <footer className='bg-blue-950 text-white bottom-0 fixed w-full py-8 font-sans shadow-[0_-4px_6px_-1px_rgba(0,0,0,0.1)]'>
+      <div className='container mx-auto px-8'>
+        <div className='grid grid-cols-3 gap-8 mb-8'>
+          {/* Navigation Section */}
+          <div className=''>
+            <h3 className='text-white mb-6 font-semibold text-xl'>
+              Navigations
+            </h3>
+            <div className='flex flex-col gap-4'>
+              {footerElements.map((item) => (
+                <Link
+                  key={item.link}
+                  to={item.link}
+                  className='text-gray-400 hover:text-white text-sm'
+                >
+                  {item.linkName}
+                </Link>
+              ))}
+            </div>
+          </div>
 
-      <div className='flex flex-col gap-3 text-gray-400 text-sm ml-5'>
-        {footerElements.map((item) => (
-          <Link to={item.link}>{item.linkName}</Link>
-        ))}
-      </div>
+          {/* Social Icons Section */}
+          <div className=''>
+            <h3 className='text-white mb-6 font-semibold text-xl'>Follow Us</h3>
+            <div className='flex flex-col gap-4'>
+              {socialIcons.map((item) => (
+                <Link
+                  key={item.name}
+                  to={item.path}
+                  className='flex items-center gap-3 text-gray-400 hover:text-white text-sm'
+                >
+                  <span className='text-lg'>{item.icon}</span>
+                  <span>{item.name}</span>
+                </Link>
+              ))}
+            </div>
+          </div>
 
-      <div className='container mx-auto text-center mt-8 '>
-        <p>Copyright &copy; {footerYear} All Rights Reserved </p>
+          {/* Location Section */}
+          <div className=''>
+            <h3 className='text-white mb-6 font-semibold text-xl'>Locate Us</h3>
+            <div className='flex flex-col gap-4 text-gray-400 text-sm'>
+              <span>New Rd, Madina</span>
+              <span>Brigadier General Ran, Agbamasu Avenue, Accra</span>
+            </div>
+          </div>
+        </div>
+
+        {/* Copyright Section */}
+        <div className='text-center text-gray-400 border-t border-gray-800 pt-4 text-sm'>
+          <p>Copyright &copy; {footerYear} All Rights Reserved </p>
+        </div>
       </div>
     </footer>
   )
