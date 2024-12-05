4fdf18c75e5f5e0eccda233d2d22ec565a5fde63
Kamasah Edwin Jnr <kamassahe5@gmail.com>
2024-12-05 13:19:28 +0000

    renamed carousel to hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -37,10 +37,10 @@ const Carousel = () => {
 
   return (
     <>
-      <div>
-        <header>
-          Quick and Reliable Repairs for Your{' '}
-          <span>
+      <div className='flex flex-col justify-center text-center items-center my-10'>
+        <header className='font-sans font-light text-2xl text-blue-800'>
+          Quick and Reliable Repairs for Your <br className='' />
+          <span className='font-bold '>
             <ReactTyped
               strings={[
                 'Apple Devices !',
@@ -48,19 +48,23 @@ const Carousel = () => {
                 'Macbooks !',
                 'iPads !',
               ]}
-              typeSpeed={100}
+              typeSpeed={150}
               backSpeend={80}
               loop
             />
           </span>
         </header>
+        <p className='mt-7 text-gray-600 text-lg'>
+          We offer fast, reliable repairs for iPhones, iPads, and MacBooks,
+          ensuring quality service and quick turnaround.
+        </p>
       </div>
 
-      <div className='w-[350px] h-[250px] bg-red-500 md:max-w-[1400px] md:h-[580px] md:w-full m-auto relative group'>
-        {/* <div
+      <div className='w-full h-[250px] bg-red-500 md:max-w-[1400px] md:h-[580px] md:w-full m-auto relative group'>
+        <div
           className='w-full h-full bg-center bg-cover duration-500'
           style={{ backgroundImage: `url(${slides[index].url})` }}
-        /> */}
+        />
         {/* Dot Navigation */}
         <div className='absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2'>
           {slides.map((_, slideIndex) => (
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,10 @@
-import Carousel from '../components/Carousel'
+import Hero from '../components/Hero'
 
 const Home = () => {
   return (
     <>
       <div className=' m-3'>
-        <Carousel />
+        <Hero />
       </div>
     </>
   )
