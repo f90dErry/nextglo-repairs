51d7fa61599e69abcb906ab4774aca8408605aa7
Kamasah Edwin Jnr <kamassahe5@gmail.com>
2025-04-02 17:24:08 +0000

    experience component revamp

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,18 +2,22 @@ import { experience } from '../data'
 
 const Experience = () => {
   return (
-    <div className='py-8 md:py-12 px-4 md:px-44'>
-      <h1>Experience the best Gadget Repair Service in Ghana</h1>
+    <div className='py-8 md:py-12 px-3 md:px-44'>
+      <h1 className='text-2xl font-bold text-center text-blue-800 pb-2'>
+        Experience the best Gadget Repair Service in Ghana
+      </h1>
 
-      {experience.map((item, index) => (
-        <div key={index} className='flex gap-3 flex-col md:flex-row'>
-          <div className='flex flex-col items-center justify-center gap-4 p-4 bg-red-600 '>
-            <item.icon className='text-6xl rounded-full p-2 text-blue-800 bg-blue-200' />
-            <h4>{item.title}</h4>
-            <p className='text-center'>{item.text}</p>
+      <div className='flex flex-col md:flex-row gap-4'>
+        {experience.map((item, index) => (
+          <div key={index} className='md:w-1/4 h-auto'>
+            <div className='flex flex-col items-center justify-center gap-2 px-4 py-6 bg-slate-600'>
+              <item.icon className='text-6xl rounded-full p-2 text-blue-800 bg-blue-200' />
+              <h4 className='font-semibold'>{item.title}</h4>
+              <p className='text-center'>{item.text}</p>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   )
 }
